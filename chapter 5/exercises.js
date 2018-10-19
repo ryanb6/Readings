52ba1d7507929949b49479b7eadd14f2ad71ddf2
rfbrady <rfbrady@email.wm.edu>
2018-10-18 22:30:49 -0400

    ch 5 exercises

--- chapter 5/exercises.js
+++ chapter 5/exercises.js
@@ -14,3 +14,29 @@ console.log(arr.reduce((a, b) => a.concat(b)));
 //starts from the beginning.
 //When defining the function, you can use a regular loop to do the actual
 //looping.
+function loop(start, test, update, body){
+  for(let value = start; test(value); value = update(value)){
+    body(value);
+  }
+}
+loop(0, n => n < 10, n => n + 1, console.log);
+
+// Everything
+// every returns true when a function returns true for every element in an array
+//implement every as a function that takes an array and a predicate function
+// write one using a loop and one using the some method
+
+let arr2 = [1,2,3,4,5];
+
+function every1(array, test){
+  for (let element of array){
+    if (!test(element)) return false;
+  }
+  return true;
+}
+function every2(array, test){
+  return !array.some(element => !test(element));
+}
+
+console.log(every1(arr2, a => a > 0));
+console.log(every2(arr2, a => a > 0));
